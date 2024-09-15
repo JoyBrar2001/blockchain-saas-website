99533fd7de8a9de33fa0286ce2b08760aac29f3f
Joy Brar <joybrar2001@gmail.com>
2024-09-15 12:24:19 +0530

    Latest Posts

--- src/sections/FeaturesCardsSection.tsx
+++ src/sections/FeaturesCardsSection.tsx
@@ -1,5 +1,6 @@
 import { twMerge } from "tailwind-merge";
 import { TextButton } from "../components/TextButton";
+import { Card } from "../components/Card";
 
 const cardData = [
   {
@@ -38,27 +39,12 @@ export const FeaturesCardsSection = () => {
 
         <div className="mt-36 lg:mt-48 flex">
           <div className="flex flex-none gap-8">
-            {cardData.map((card, index) => (
-              <div
+            {cardData.map(({image, title, description, color}, index) => (
+              <Card
                 key={index}
-                className="relative z-0 p-8 md:p-10 max-w-xs group"
+                className="max-w-xs"
+                color={color}
               >
-                <div className={twMerge(
-                  "absolute size-1/6 rounded-xl bg-fuchsia-500 top-1.5 right-1.5 -z-10 blur-lg opacity-0 group-hover:opacity-100 transition duration-300",
-                  card.color === "lime" && "bg-lime-500",
-                  card.color === "cyan" && "bg-cyan-500",
-                  card.color === "violet" && "bg-violet-500",
-                )}></div>
-
-                <div className={twMerge(
-                  "absolute size-1/6 rounded-xl bg-fuchsia-500 group-hover:bg-fuchsia-400 transition duration-300 top-1.5 right-1.5 -z-10",
-                  card.color === "lime" && "bg-lime-500 group-hover:bg-lime-400",
-                  card.color === "cyan" && "bg-cyan-500 group-hover:bg-cyan-400",
-                  card.color === "violet" && "bg-violet-500 group-hover:bg-violet-400",
-                )}></div>
-
-                <div className="absolute inset-0 bg-zinc-800 -z-10 rounded-2xl [mask-image:linear-gradient(225deg,transparent,transparent_40px,black_40px)]"></div>
-
                 <div className="flex justify-center -mt-28">
                   <div className="inline-flex relative">
                     <div
@@ -67,7 +53,7 @@ export const FeaturesCardsSection = () => {
                     ></div>
 
                     <img
-                      src={card.image}
+                      src={image}
                       alt="Pill Image"
                       className="size-40 group-hover:-translate-y-6 transition duration-300"
                     />
@@ -75,34 +61,13 @@ export const FeaturesCardsSection = () => {
                 </div>
 
                 <h3 className="font-heading font-black text-3xl mt-12">
-                  {card.title}
+                  {title}
                 </h3>
 
                 <p className="text-lg text-zinc-400 mt-4">
-                  {card.description}
+                  {description}
                 </p>
-
-                <div className="flex justify-between mt-12">
-                  <TextButton color={card.color}>
-                    Learn More
-                  </TextButton>
-
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width={2}
-                    stroke="currentColor"
-                    className="size-8 text-zinc-500 group-hover:text-zinc-300 -translate-x-2 group-hover:translate-x-0 transition duration-300"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-                    />
-                  </svg>
-                </div>
-              </div>
+              </Card>
             ))}
           </div>
         </div>
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,31 +1,79 @@
 import { CutCornerButton } from "../components/CutCornerButton"
+import { Hexagon } from "../components/Hexagon";
+
+const navLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
 
 export const HeaderSection = () => {
   return (
-    <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg z-50">
-      <div className="container">
-        <div className="flex justify-between items-center h-24 md:h-28">
-          <div>
-            <img src="/assets/images/logo.svg" alt="Blockforge Logo" />
-          </div>
+    <>
+      <header className="sticky top-0 bg-zinc-900/50 backdrop-blur-lg z-40">
+        <div className="container">
+          <div className="flex justify-between items-center h-24 md:h-28">
+            <div>
+              <img src="/assets/images/logo.svg" alt="Blockforge Logo" />
+            </div>
 
-          <div className="flex gap-4 items-center">
-            <CutCornerButton className="hidden md:inline-flex">
-              Get Started
-            </CutCornerButton>
+            <div className="flex gap-4 items-center">
+              <CutCornerButton className="hidden md:inline-flex">
+                Get Started
+              </CutCornerButton>
 
-            <div className="size-10 relative">
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
-              </div>
+              <div className="size-10 relative">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 -translate-y-1"></div>
+                </div>
 
-              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                  <div className="w-5 h-0.5 bg-zinc-300 translate-y-1"></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {/* <div className="fixed size-full top-0 left-0 z-30 bg-zinc-900">
+        <div className="absolute inset-2 rounded-md md:rounded-lg lg:rounded-xl bg-zinc-800 mt-28 z-0 overflow-hidden">
+          <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
+            <Hexagon className="size-[700px]" />
+          </div>
+
+          <div className="absolute top-1/2 left-full -translate-y-1/2 -translate-x-1/2 -z-10">
+            <Hexagon className="size-[1100px]" />
+          </div>
+
+          <div className="h-full flex justify-center items-center">
+            <nav className="flex flex-col items-center gap-12">
+              {navLinks.map(({ title, href }) => (
+                <a
+                  className="text-4xl font-heading font-black text-zinc-500 hover:text-zinc-300 transition duration-300"
+                  href={href}
+                  key={title}
+                >
+                  {title}
+                </a>
+              ))}
+            </nav>
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 }
